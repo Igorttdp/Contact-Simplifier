87475071437ca0dff97e6edf74fd21b71a62985d
Igorttdp <igorttdp.dev@email.com>
2023-03-30 20:08:49 -0300

    feat: add 'Providers' component

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,19 +1,11 @@
-import { UserProvider } from "@/context/userContext";
-import { RegisterProvider } from "@/context/registerContext";
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-
-const queryClient = new QueryClient();
+import Providers from "@/context";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <QueryClientProvider client={queryClient}>
-      <UserProvider>
-        <RegisterProvider>
-          <Component {...pageProps} />
-        </RegisterProvider>
-      </UserProvider>
-    </QueryClientProvider>
+    <Providers>
+      <Component {...pageProps} />
+    </Providers>
   );
 }
