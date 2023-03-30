7aec28130af83cef3a0f4f65aa1307e8adefe33b
Igorttdp <igorttdp.dev@email.com>
2023-03-30 02:47:07 -0300

    feat: add Providers
    Add QueryClient instance and QueryClientProviders for handle promises. Also, add User and Register provider to manage the app dinamically

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,19 @@
+import { UserProvider } from "@/context/userContext";
+import { RegisterProvider } from "@/context/registerContext";
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const queryClient = new QueryClient();
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <QueryClientProvider client={queryClient}>
+      <UserProvider>
+        <RegisterProvider>
+          <Component {...pageProps} />
+        </RegisterProvider>
+      </UserProvider>
+    </QueryClientProvider>
+  );
 }
