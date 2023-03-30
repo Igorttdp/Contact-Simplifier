58e691a1841e11c02d47a3a07608e02b268b4e8e
Igorttdp <igorttdp.dev@email.com>
2023-03-30 20:50:40 -0300

    feat: add updateUserDate function to update an user
    This function handle a little bit more the data provided. On line 49 to 64, filledData calls the cunstructor 'entries' method, then pass the 'payload' as prop to run a reduce that will check if the value is filled or not. Also, do a additional verification if the key is 'phone' to replace the masked data to a normalized string of numbers. Then, the api request is triggered calling '/profile' in URL and passing 'filledData' as props.

--- src/context/userContext.tsx
+++ src/context/userContext.tsx
@@ -2,15 +2,17 @@ import { ILoginRequestProps } from "@/interfaces/login.interface";
 import api from "@/services/api";
 import { useRouter } from "next/router";
 import { createContext, Dispatch, SetStateAction, useState } from "react";
-import { setCookie, destroyCookie } from "nookies";
+import { setCookie, destroyCookie, parseCookies } from "nookies";
 import { IProviderProps } from "@/interfaces/provider.interface";
 import { IUserProfile } from "@/interfaces/user.interface";
+import { IUpdateUserRequestProps } from "@/interfaces/register.interface";
 
 interface UserProviderData {
   login(data: ILoginRequestProps): Promise<string>;
   logout(): void;
   profile: IUserProfile;
   setProfile: Dispatch<SetStateAction<IUserProfile>>;
+  updateUserData(payload: IUpdateUserRequestProps): Promise<IUserProfile>;
 }
 
 export const UserContext = createContext<UserProviderData>(
@@ -41,8 +43,37 @@ export const UserProvider = ({ children }: IProviderProps) => {
     router.push("/");
   };
 
+  const updateUserData = async (payload: IUpdateUserRequestProps) => {
+    const cookies = parseCookies();
+
+    const filledData: IUpdateUserRequestProps = Object.entries(payload).reduce(
+      (acc, [key, value]: [string, string]) => {
+        if (value !== "") {
+          acc[key as keyof IUpdateUserRequestProps] = value;
+
+          if (key === "phone") {
+            acc[key as keyof IUpdateUserRequestProps] = value.replace(
+              /[^$0-9\.]/g,
+              ""
+            );
+          }
+        }
+        return acc;
+      },
+      {} as IUpdateUserRequestProps
+    );
+
+    return await api
+      .patch("/profile", filledData, {
+        headers: { Authorization: `Bearer ${cookies["token"]}` },
+      })
+      .then((res) => res.data);
+  };
+
   return (
-    <UserContext.Provider value={{ login, logout, profile, setProfile }}>
+    <UserContext.Provider
+      value={{ login, logout, profile, setProfile, updateUserData }}
+    >
       {children}
     </UserContext.Provider>
   );
