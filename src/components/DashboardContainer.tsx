80ce503dd0917ab4ab0a7ae03b05aac25d249571
Igorttdp <igorttdp.dev@email.com>
2023-03-30 03:22:26 -0300

    feat: add DashboardContainer styled component

---
+++ src/components/DashboardContainer.tsx
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+const DashboardContainer = styled.div`
+  width: 100%;
+  min-height: 100vh;
+  display: flex;
+  flex-flow: row nowrap;
+  justify-content: space-between;
+  align-items: flex-start;
+  padding: 10rem;
+`;
+
+export default DashboardContainer;
