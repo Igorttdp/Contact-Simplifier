454b01e55c7726e6faae411c9a9772c5691b6b13
Igorttdp <igorttdp.dev@email.com>
2023-03-30 03:12:11 -0300

    feat: add custom Dialog.Content

---
+++ src/components/Modal/Content.tsx
@@ -0,0 +1,42 @@
+import { DialogContent } from "@radix-ui/react-dialog";
+import styled from "styled-components";
+
+const ModalContent = styled(DialogContent)`
+  background-color: #333a41;
+  border-radius: 6px;
+  width: 90vw;
+  max-width: 450px;
+  height: 75rem;
+  padding: 2.8rem 5.5rem;
+  position: absolute;
+  display: flex;
+  flex-flow: column nowrap;
+  align-items: center;
+  justify-content: center;
+
+  form {
+    display: flex;
+    flex-flow: column nowrap;
+    align-items: center;
+    justify-content: center;
+    gap: 3.2rem;
+
+    >h2 {
+      font-size: 2.6rem;
+    }
+  }
+
+  >div {
+    display: flex;
+    flex-flow: column nowrap;
+    align-items: center;
+    justify-content: center;
+    gap: 4rem;
+
+    span {
+      font-size: 3.5rem;
+      font-weight: bold;
+    }
+  }
+`;
+export default ModalContent;
