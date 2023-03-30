4a0f23a8e3cd7f54b2ab70796dd9ed531c5f1d31
Igorttdp <igorttdp.dev@email.com>
2023-03-30 02:51:13 -0300

    feat: add homepage
    Add some hook states to handle page dinamically. [tokenExists] check if token exists to grant permission to enter in dashboard page with 'clientRendered' state. Open is used to handle 'Modal' state.

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,18 +1,43 @@
 import FullWidthCentralizeContainer from "@/components/FullWidthCentralizeContainer";
 import LoginForm from "@/components/LoginFormContainer";
+import { NextPage } from "next";
 import Image from "next/image";
+import { parseCookies } from "nookies";
+import { useEffect, useState } from "react";
+import Modal from "@/components/Modal";
+import RegisterFormContainer from "@/components/RegisterFormContainer";
 
-export default function Home() {
+const Home: NextPage = () => {
+  const cookies = parseCookies();
+
+  const [clientRendered, setClientRendered] = useState(false);
+  const [tokenExits, setTokenExists] = useState(Boolean(cookies["token"]));
+  const [open, setOpen] = useState(false);
+
+  useEffect(() => {
+    setClientRendered(true);
+  }, []);
 
   return (
     <FullWidthCentralizeContainer>
       <Image
         src="Logo.svg"
         alt={"Contact Simplifier Logo"}
-        width={472}
-        height={162}
+        width={372}
+        height={120}
+        priority
       />
-      <LoginForm />
+      <LoginForm
+        setOpen={setOpen}
+        tokenExists={tokenExits}
+        setTokenExists={setTokenExists}
+        clientRendered={clientRendered}
+      />
+      <Modal open={open} setOpen={setOpen}>
+        <RegisterFormContainer setOpen={setOpen} />
+      </Modal>
     </FullWidthCentralizeContainer>
   );
-}
+};
+
+export default Home;
