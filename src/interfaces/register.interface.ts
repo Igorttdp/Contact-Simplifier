3b50fc5b4f1802807a97d0af48fe4008031660ab
Igorttdp <igorttdp.dev@email.com>
2023-03-30 20:30:33 -0300

    feat: add IUpdateUserRequestProps interface

--- src/interfaces/register.interface.ts
+++ src/interfaces/register.interface.ts
@@ -1,9 +1,17 @@
 import { ILoginRequestProps } from "./login.interface";
 
 export interface IRegisterRequestProps {
-    name: string;
-    email: string;
-    password: string;
-    confirmPassword: string;
-    phone: string;
+  name: string;
+  email: string;
+  password: string;
+  confirmPassword: string;
+  phone: string;
+}
+
+export interface IUpdateUserRequestProps {
+  name?: string;
+  email?: string;
+  password?: string;
+  confirmPassword?: string;
+  phone?: string;
 }
