b36cdae42467f6f071709caee0138b4909c939b1
Igorttdp <igorttdp.dev@email.com>
2023-03-30 03:13:50 -0300

    feat: add new style options using passed props

--- src/components/OutlineButton.tsx
+++ src/components/OutlineButton.tsx
@@ -1,16 +1,35 @@
+import { ReactNode } from "react";
 import styled from "styled-components";
 
-const OutlineButton = styled.button`
+interface IOutlineButtonProps {
+  mode?: string;
+  width?: string;
+}
+
+const OutlineButton = styled.button<IOutlineButtonProps>`
   cursor: pointer;
+  width: ${({ width }) => (width ? width : "100%")};
   color: #ffffff;
   background-color: transparent;
-  border: 1px solid #008c62;
-  font-size: 2rem;
-  padding: 1.2rem 9.6rem;
+  border: ${({ mode }) =>
+    mode === "normal"
+      ? "1px solid #007DA5"
+      : mode === "sucess"
+      ? "1px solid #008c62"
+      : "1px solid #BA0101"};
+  font-size: 1.6rem;
+  padding: 1.2rem 1rem;
   border-radius: 0.4rem;
+  transition: all 0.3s;
 
-  :hover {
-    background-color: #008c62;
+  &:hover {
+    background-color: ${({ mode }) =>
+      mode === "normal"
+        ? "#007DA5"
+        : mode === "sucess"
+        ? "#008c62"
+        : "#BA0101"};
+    transition: all 0.3s;
   }
 `;
 
