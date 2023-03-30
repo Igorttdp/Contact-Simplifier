62a1800886a6e2df382d120d5ea4319524d14ef8
Igorttdp <igorttdp.dev@email.com>
2023-03-30 03:13:31 -0300

    feat: add new style options using passed props

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -1,23 +1,40 @@
-import styled from "styled-components";
+import { FieldError, FieldErrors, UseFormRegister } from "react-hook-form";
+import { styled } from "styled-components";
 
-const StyledInput = styled.input`
+interface StyledInputProps {
+  error?: FieldError;
+  filled?: string;
+  width?: string;
+}
+
+const StyledInput = styled.input<StyledInputProps>`
   color: #ffffff;
   background-color: #222426;
   padding: 1.6rem 4rem;
-  font-size: 2rem;
-  width: 430px;
+  font-size: 1.4rem;
+  width: ${({ width }) => (!width ? "340px" : width)};
   flex-direction: row;
   align-items: center;
   border-radius: 10px;
   outline: 0;
-  border: 1px solid transparent;
+  border: ${({ error, filled }) =>
+    error
+      ? "1px solid #BA0101"
+      : filled
+      ? "1px solid #008C62"
+      : "1px solid transparent"};
 
-  ::placeholder {
-    background-color: #6b6b6b;
+  &::placeholder {
+    color: #6b6b6b;
   }
 
-  :focus-visible {
-    border-color: #0093c1;
+  &:focus-visible {
+    border-color: ${({ error, filled }) =>
+      error ? " #ff5100" : filled ? "#00d495" : "#0093c1"};
+  }
+
+  &:-webkit-autofill {
+    background-color: transparent !important;
   }
 `;
 
