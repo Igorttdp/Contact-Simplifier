6765761782bc186ef4955e3387b8bcd7aae524a0
Igorttdp <igorttdp.dev@email.com>
2023-03-30 22:28:00 -0300

    feat: add ToastContainer component

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,14 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import Providers from "@/context";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <Providers>
       <Component {...pageProps} />
+      <ToastContainer />
     </Providers>
   );
 }
