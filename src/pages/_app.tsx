c587ca027ce6709fb6b5f1edfa949b588b6cfb0a
Igorttdp <igorttdp.dev@email.com>
2023-03-31 11:27:50 -0300

    feat: add title tag and sets the value for 'Contact Simplifier'

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,10 +3,14 @@ import type { AppProps } from "next/app";
 import Providers from "@/context";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
+import Head from "next/head";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <Providers>
+      <Head>
+        <title>Contact Simplifier</title>
+      </Head>
       <Component {...pageProps} />
       <ToastContainer />
     </Providers>
