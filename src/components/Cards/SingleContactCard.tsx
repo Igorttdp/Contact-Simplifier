0c64aaa3684a34fa633fbf434daa74be58a83948
Igorttdp <igorttdp.dev@email.com>
2023-03-31 11:38:07 -0300

    feat: add deleteContact function
    The function is triggered by clicking in button, then the callback function send the contact id to request a deletion. In case of success or error, a toast will be fired with a custom message

--- src/components/Cards/SingleContactCard.tsx
+++ src/components/Cards/SingleContactCard.tsx
@@ -1,6 +1,10 @@
 import { IContactResponseProps } from "@/interfaces/contact.interface";
 import Image from "next/image";
 import styled from "styled-components";
+import { useContext, useState } from "react";
+import { useMutation } from "@tanstack/react-query";
+import { ContactsContext } from "@/context/contactsContext";
+import { toast } from "react-toastify";
 
 const SingleContentCardContainer = styled.li`
   background-color: #2f3133;
@@ -11,9 +15,25 @@ const SingleContentCardContainer = styled.li`
   flex-flow: row nowrap;
   align-items: center;
   gap: 3.2rem;
+  position: relative;
 
   p {
     font-size: 1.8rem;
+  }
+
+  > button {
+    background-image: url(/trash.png);
+    background-repeat: no-repeat;
+    background-position: center center;
+    background-color: transparent;
+    cursor: pointer;
+    border: none;
+    outline: 0;
+
+    width: 21px;
+    height: 24px;
+    position: absolute;
+    right: 2rem;
   }
 `;
 
@@ -22,6 +42,19 @@ interface ISingleContactCardProps {
 }
 
 const SingleContactCard = ({ contact }: ISingleContactCardProps) => {
+  const { deleteContact, contacts, setContacts } = useContext(ContactsContext);
+
+  const { mutateAsync } = useMutation({
+    mutationFn: deleteContact,
+    onSuccess: () => toast.success("Deletado com sucesso!"),
+    onError: () => toast.error("Ops, algo deu errado"),
+  });
+
+  const onDelete = async () => {
+    await mutateAsync(contact.id).catch(() => {});
+    setContacts(contacts.filter((el) => el.id !== contact.id));
+  };
+
   return (
     <SingleContentCardContainer>
       <Image
@@ -31,6 +64,7 @@ const SingleContactCard = ({ contact }: ISingleContactCardProps) => {
         height={28}
       />
       <p>{contact.name}</p>
+      <button onClick={onDelete}></button>
     </SingleContentCardContainer>
   );
 };
