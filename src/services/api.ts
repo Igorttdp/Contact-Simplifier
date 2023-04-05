c836cb1697addbb836463fd645a140016c69347a
Igorttdp <igorttdp.dev@email.com>
2023-04-05 12:57:07 -0300

    feat: add new production api url

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:3003",
+  baseURL: "https://contact-simplifier-api.onrender.com",
   timeout: 3000,
   withCredentials: true,
 });
