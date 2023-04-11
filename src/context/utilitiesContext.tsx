725ca3ec367221f48521b7ea840d5e16543ee251
Igorttdp <igorttdp.dev@email.com>
2023-04-11 16:19:17 -0300

    feat: add changeButtonState
    This function change the inner content of the 'OutlineButton' for a loader

--- src/context/utilitiesContext.tsx
+++ src/context/utilitiesContext.tsx
@@ -1,10 +1,13 @@
+import OutlineButton from "@/components/OutlineButton";
 import { IProviderProps } from "@/interfaces/provider.interface";
 import moment from "moment";
-import { createContext, Dispatch, SetStateAction } from "react";
+import { createContext } from "react";
+import { ThreeDots } from "react-loader-spinner";
 
 interface IUtilitiesProviderProps {
   phoneMask(value: string): string;
   handleDate(date: Date | string): string;
+  changeButtonState(isLoading: boolean, innerText: string): JSX.Element;
 }
 
 export const UtilitiesContext = createContext<IUtilitiesProviderProps>(
@@ -22,8 +25,35 @@ const UtilitiesProvider = ({ children }: IProviderProps) => {
 
   const handleDate = (date: Date | string) => moment(date).format("DD/MM/YYYY");
 
+  const changeButtonState = (isLoading: boolean, innerText: string) => {
+    console.log(isLoading);
+
+    if (isLoading)
+      return (
+        <OutlineButton width={"100%"} mode={"sucess"}>
+          <ThreeDots
+            height="22"
+            width="22"
+            radius="9"
+            color="#4fa94d"
+            ariaLabel="three-dots-loading"
+            wrapperStyle={{ justifyContent: "center" }}
+            visible={true}
+          />
+        </OutlineButton>
+      );
+
+    return (
+      <OutlineButton width={"100%"} mode={"sucess"}>
+        {innerText}
+      </OutlineButton>
+    );
+  };
+
   return (
-    <UtilitiesContext.Provider value={{ phoneMask, handleDate }}>
+    <UtilitiesContext.Provider
+      value={{ phoneMask, handleDate, changeButtonState }}
+    >
       {children}
     </UtilitiesContext.Provider>
   );
