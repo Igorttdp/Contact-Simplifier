0e27b938bb6da74c4e72c71b187ff995f5f17dc2
Igorttdp <igorttdp.dev@email.com>
2023-04-11 14:19:30 -0300

    style: add grid display for small screens (notebooks, laptops & mobiles devices)

--- src/components/DashboardContainer.tsx
+++ src/components/DashboardContainer.tsx
@@ -6,8 +6,27 @@ const DashboardContainer = styled.main`
   display: flex;
   flex-flow: row nowrap;
   justify-content: space-between;
+  gap: 2rem;
   align-items: flex-start;
   padding: 10rem;
+
+  @media (max-width: 1440px) {
+    padding: 8% 5%;
+    grid-row-gap: 5rem;
+    grid-column-gap: 3rem;
+    display: grid;
+    grid-template-rows: auto;
+    grid-template-columns: repeat(2, 1fr);
+    grid-template-areas: "pc pc" "cc ac";
+    align-items: start;
+    justify-items: center;
+  }
+
+  @media (max-width: 1024px) {
+    grid-template-rows: repeat(3, auto);
+    grid-template-columns: 1fr;
+    grid-template-areas: "pc" "cc" "ac";
+  }
 `;
 
 export default DashboardContainer;
