2baa7b1c3e877ef8c00d91be907322a19cda570e
Igorttdp <igorttdp.dev@email.com>
2023-04-11 15:16:55 -0300

    feat: add media queries and styles for form scroll-bar

--- src/components/Modal/Content.tsx
+++ src/components/Modal/Content.tsx
@@ -6,7 +6,8 @@ const ModalContent = styled(DialogContent)`
   border-radius: 6px;
   width: 90vw;
   max-width: 450px;
-  height: 75rem;
+  max-height: 75rem;
+  height: 100%;
   padding: 2.8rem 5.5rem;
   position: absolute;
   display: flex;
@@ -14,15 +15,49 @@ const ModalContent = styled(DialogContent)`
   align-items: center;
   justify-content: center;
 
+  @media (max-width: 425px) {
+    padding: 2.5rem 1.5rem;
+  }
+
+  @media (max-width: 320px) {
+    padding: 2.5rem 0.5rem;
+  }
+
+  @media (max-height: 750px) {
+    height: 80vh;
+  }
+
   form {
+    width: 100%;
+    height: 66.7rem;
     display: flex;
     flex-flow: column nowrap;
     align-items: center;
     justify-content: center;
     gap: 3.2rem;
+    overflow-y: auto;
+    padding: 0 1rem;
+
+    @media (max-height: 630px) {
+      justify-content: flex-start;
+    }
 
     > h2 {
       font-size: 2.6rem;
+    }
+
+    &::-webkit-scrollbar {
+      width: 5px;
+      background-color: transparent;
+    }
+
+    &::-webkit-scrollbar-thumb {
+      background-color: #222426;
+      border-radius: 5px;
+    }
+
+    &::-webkit-scrollbar-thumb:hover {
+      background-color: #22272c;
     }
   }
 
