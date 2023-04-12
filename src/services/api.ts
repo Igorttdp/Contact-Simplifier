99926009a38692f6a8ba6d82b838a290b4698923
Igorttdp <igorttdp.dev@email.com>
2023-04-11 21:58:04 -0300

    feat: sets base url based on NODE_ENV value

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,14 @@
 import axios from "axios";
 
+const enviroment = process.env.NODE_ENV;
+
+const url: string =
+  enviroment === "production"
+    ? "https://contact-simplifier-api.onrender.com"
+    : "http://localhost:3003";
+
 const api = axios.create({
-  baseURL: "https://contact-simplifier-api.onrender.com",
+  baseURL: url,
   withCredentials: true,
 });
 
